de588b72e0c6a03ffc79933517d9b75476cbe020
szhouse <94644378@qq.com>
2018-09-04 09:34:36 +0800

    init

---
+++ src/main/java/com/szhouse/estest/EsTestApplication.java
@@ -0,0 +1,12 @@
+package com.szhouse.estest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsTestApplication.class, args);
+    }
+}
